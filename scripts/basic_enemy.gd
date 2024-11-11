5c8655924b7087cf4a361abdb37a041800403e31
MicKota <kotalamichal02@gmail.com>
2024-11-11 13:36:11 +0100

    poprawione

--- scripts/basic_enemy.gd
+++ scripts/basic_enemy.gd
@@ -1,6 +1,11 @@
 extends Node3D
+class_name Enemy
 
 @export var enemy_settings:EnemySettings
+
+var enemy_health:int
+
+signal enemy_finished
 
 var attackable:bool = false
 var distance_travelled:float = 0
@@ -9,20 +14,21 @@ var path_3d:Path3D
 var path_follow_3d:PathFollow3D
 
 func _ready():
-	print("Ready")
+	#print("Ready")
+	enemy_health=enemy_settings.health
 	$Path3D.curve = path_route_to_curve_3d()
 	$Path3D/PathFollow3D.progress = 0
 	
 
 func _on_spawning_state_entered():
-	print("Spawning")
+	#print("Spawning")
 	attackable = false
 	$AnimationPlayer.play("spawn")
 	await $AnimationPlayer.animation_finished
 	$EnemyStateChart.send_event("to_travelling_state")
 
 func _on_travelling_state_entered():
-	print("Travelling")
+	#print("Travelling")
 	attackable = true
 
 func _on_travelling_state_processing(delta):
@@ -34,13 +40,13 @@ func _on_travelling_state_processing(delta):
 		$EnemyStateChart.send_event("to_damaging_state")
 
 func _on_despawning_state_entered():
-	print("Despawning")
+	enemy_finished.emit()
 	$AnimationPlayer.play("despawn")
 	await $AnimationPlayer.animation_finished
 	$EnemyStateChart.send_event("to_remove_enemy_state")
 
 func _on_remove_enemy_state_entered():
-	print("queue_free()")
+	#print("queue_free()")
 	queue_free()
 
 func _on_damaging_state_entered():
@@ -49,7 +55,10 @@ func _on_damaging_state_entered():
 	$EnemyStateChart.send_event("to_despawning_state")
 
 func _on_dying_state_entered():
-	print("Playing a dying animation!")
+	enemy_finished.emit()
+	$ExplosionAudio.play()
+	$Path3D/PathFollow3D/enemy_ufoRed2.visible = false
+	await $ExplosionAudio.finished
 	$EnemyStateChart.send_event("to_remove_enemy_state")
 	
 func path_route_to_curve_3d() -> Curve3D:
@@ -59,3 +68,11 @@ func path_route_to_curve_3d() -> Curve3D:
 		c3d.add_point(Vector3(element.x, 0.25, element.y))
 
 	return c3d
+
+
+func _on_area_3d_area_entered(area):
+	if area is Projectile:
+		enemy_health -= area.damage
+	
+	if enemy_health <= 0:
+		$EnemyStateChart.send_event("to_dying_state")
