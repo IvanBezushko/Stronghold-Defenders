84f2939077db3cf314ffc6a5f08d46815d5eed28
MicKota <kotalamichal02@gmail.com>
2024-12-04 12:36:10 +0100

    poprawione wieże
    nie można stawiać wież w tym samym miejscu

--- scripts/activity_button_3.gd
+++ scripts/activity_button_3.gd
@@ -11,6 +11,8 @@ var _is_valid_location:bool = false
 var _last_valid_location:Vector3
 var _cam:Camera3D
 var RAYCAST_LENGTH:float = 100
+var occupied_positions: Array = []
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -37,10 +39,14 @@ func _physics_process(_delta):
 		if rayResult.size() > 0:
 			var co:CollisionObject3D = rayResult.get("collider")
 			if "grid_empty" in str(co.get_groups()):
-				_draggable.visible = true
-				_is_valid_location = true
 				_last_valid_location = Vector3(co.global_position.x, 0.2, co.global_position.z)
-				_draggable.global_position = _last_valid_location
+				if not _is_position_occupied(_last_valid_location):
+					_draggable.visible = true
+					_is_valid_location = true
+					_draggable.global_position = _last_valid_location
+				else:
+					_draggable.visible = false
+					_is_valid_location = false
 		else:
 			_draggable.visible = false
 
@@ -59,3 +65,10 @@ func _on_button_up():
 		add_child(activity)
 		activity.global_position = _last_valid_location
 		main.cash-=activity_cost
+		occupied_positions.append(_last_valid_location)
+
+func _is_position_occupied(position: Vector3) -> bool:
+	for occupied in occupied_positions:
+		if position.distance_to(occupied) < 0.5: 
+			return true
+	return false
